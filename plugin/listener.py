f020195c734b3c867fbfd0f641523490a8c6513e
Jack Cherng <jfcherng@gmail.com>
2022-10-19 21:13:35 +0800

    refactor: update template variables only when necessary
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/listener.py
+++ plugin/listener.py
@@ -1,32 +1,52 @@
+from typing import Any, Dict
+
 import sublime
 import sublime_plugin
 
 from .constant import PLUGIN_NAME, STATUS_MSG_KEY
-from .utils import get_plugin_setting, get_template_variables, is_regular_view
+from .utils import get_plugin_setting, is_regular_view
 
 
 class CustomStatusMessageListener(sublime_plugin.ViewEventListener):
     def __init__(self, view: sublime.View, *args, **kwargs) -> None:
         super().__init__(view, *args, **kwargs)
         self.v_settings = view.settings()
-        self.v_settings.add_on_change(PLUGIN_NAME, self._work)
+        self.v_settings.add_on_change(PLUGIN_NAME, self._update_all_variables)
+
+        self.variables: Dict[str, Any] = {}
+        self._update_all_variables()
 
     def __del__(self) -> None:
         self.v_settings.clear_on_change(PLUGIN_NAME)
 
+    def on_activated_async(self) -> None:
+        self._update_variables_on_view_activated()
+        self._update_status_message()
+
+    on_post_move_async = on_activated_async
+
+    def on_selection_modified_async(self) -> None:
+        self._update_variables_on_selection_changed()
+        self._update_status_message()
+
+    def on_reload_async(self) -> None:
+        self._update_all_variables()
+        self._update_status_message()
+
+    on_associate_buffer_async = on_reload_async
+    on_revert_async = on_reload_async
+    on_post_save_async = on_reload_async
+
     def _is_enabled(self, view: sublime.View) -> bool:
         return bool(get_plugin_setting("enabled") and is_regular_view(view) and len(view.sel()) == 1)
 
-    def _work(self) -> None:
+    def _update_status_message(self) -> None:
         if not self._is_enabled(self.view):
             self.view.erase_status(STATUS_MSG_KEY)
             return
 
-        template = str(get_plugin_setting("template"))
-        variables = get_template_variables(self.view, point=self.view.sel()[0].b)
-
         try:
-            msg = template.format_map(variables)
+            msg = str(get_plugin_setting("template")).format_map(self.variables)
         except KeyError as e:
             msg = f"[{PLUGIN_NAME}] Unknown template variable: {e}"
         except Exception as e:
@@ -34,5 +54,47 @@ class CustomStatusMessageListener(sublime_plugin.ViewEventListener):
 
         self.view.set_status(STATUS_MSG_KEY, msg)
 
-    on_activated_async = _work
-    on_selection_modified_async = _work
+    def _update_all_variables(self) -> None:
+        self._update_variables_on_view_activated()
+        self._update_variables_on_selection_changed()
+
+    def _update_variables_on_selection_changed(self) -> None:
+        point = self.view.sel()[0].b  # caret's visual position
+        row_0, col_0 = self.view.rowcol(point)
+        row_1, col_1 = row_0 + 1, col_0 + 1
+        scope = self.view.scope_name(point).strip()
+
+        self.variables.update(
+            {
+                # row/col/point
+                "row": row_1,
+                "col": col_1,
+                "row_0": row_0,
+                "col_0": col_0,
+                "point": point,
+                # scope
+                "scope": scope,
+                "base_scope": scope.partition(" ")[0],
+                "last_scope": scope.rpartition(" ")[2],
+            }
+        )
+
+    def _update_variables_on_view_activated(self) -> None:
+        buffer = self.view.buffer()
+        window = self.view.window()
+        assert window
+
+        self.variables.update(
+            {
+                # containers
+                "buffer_id": buffer.id(),
+                "view_id": self.view.id(),
+                "sheet_id": self.view.sheet_id(),
+                "window_id": window.id(),
+                # others
+                "encoding": self.view.encoding(),
+                "line_endings": self.view.line_endings(),
+                "size": self.view.size(),
+                "syntax": self.view.syntax(),
+            }
+        )
--- plugin/utils.py
+++ plugin/utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any
 
 import sublime
 
@@ -15,36 +15,3 @@ def get_plugin_settings() -> sublime.Settings:
 
 def is_regular_view(view: sublime.View) -> bool:
     return bool(view.is_valid() and not view.is_loading() and not view.element() and view.window())
-
-
-def get_template_variables(view: sublime.View, *, point: int) -> Dict[str, Any]:
-    buffer = view.buffer()
-    window = view.window()
-    assert window
-
-    row_0, col_0 = view.rowcol(point)
-    row_1, col_1 = row_0 + 1, col_0 + 1
-    scope = view.scope_name(point).strip()
-
-    return {
-        # row/col/point
-        "row": row_1,
-        "col": col_1,
-        "row_0": row_0,
-        "col_0": col_0,
-        "point": point,
-        # scope
-        "scope": scope,
-        "base_scope": scope.partition(" ")[0],
-        "last_scope": scope.rpartition(" ")[2],
-        # buffer/view/sheet/window
-        "buffer_id": buffer.id(),
-        "view_id": view.id(),
-        "sheet_id": view.sheet_id(),
-        "window_id": window.id(),
-        # others
-        "encoding": view.encoding(),
-        "line_endings": view.line_endings(),
-        "size": view.size(),
-        "syntax": view.syntax(),
-    }
